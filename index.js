fe2dd4ec64bc07691bd911491a5e7e08469d4d0e
Diego Miguel <digznav@gmail.com>
2019-05-26 00:08:12 -0500

    Include all rules to pass test

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-module.exports = {};
+module.exports = {
+  extends: ['./rules/all'].map(require.resolve),
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-var': 'off',
+    eqeqeq: 'off',
+    'vars-on-top': 'off',
+    'prefer-arrow-callback': [
+      'error',
+      {
+        allowNamedFunctions: true
+      }
+    ]
+  }
+};
