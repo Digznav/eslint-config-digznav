91b463e79f444dc51d072e4bf9734d28cc974566
Diego Miguel <digznav@gmail.com>
2019-05-26 00:47:22 -0500

    Fix tests

---
+++ test/requires.js
@@ -0,0 +1,7 @@
+const test = require('tape');
+
+test('all entry points parse', function testAllEntries(t) {
+  t.doesNotThrow(function testMain() { return require('..'); }, 'index does not throw');
+
+  t.end();
+});
