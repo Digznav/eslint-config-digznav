13aa2fe62e0307b5cf6d117d2b67cb72e684d0a7
Diego Miguel <digznav@gmail.com>
2019-05-27 11:04:25 -0500

    Ensure activation of prefer-arrow-callback

--- index.js
+++ index.js
@@ -5,5 +5,15 @@ module.exports = {
     './rules/es6',
     './rules/imports',
     './rules/prettier'
-  ].map(require.resolve)
+  ].map(require.resolve),
+  rules: {
+    // In must cases use named functions.
+    // https://eslint.org/docs/rules/prefer-arrow-callback
+    'prefer-arrow-callback': [
+      'error',
+      {
+        allowNamedFunctions: true
+      }
+    ]
+  }
 };
--- rules/es6.js
+++ rules/es6.js
@@ -2,15 +2,6 @@ module.exports = {
   rules: {
     // Always prefer `var` over `let` or `const`
     // https://eslint.org/docs/rules/no-var
-    'no-var': 'off',
-
-    // In must cases use named functions.
-    // https://eslint.org/docs/rules/prefer-arrow-callback
-    'prefer-arrow-callback': [
-      'error',
-      {
-        allowNamedFunctions: true
-      }
-    ]
+    'no-var': 'off'
   }
 };
